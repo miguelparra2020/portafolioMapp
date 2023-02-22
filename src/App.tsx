2d352ca6ccd1b803616ef24b7cf538eb29d23138
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-02-21 19:00:57 -0500

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,13 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { bagCheckOutline,     cashOutline,    chatbubbleEllipsesOutline,    checkmarkDoneCircleOutline,    closeCircleOutline,    fileTrayStackedOutline,   globeOutline,   home,  } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Tab4 from './pages/Tab4';
+import Tab5 from './pages/Tab5';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -34,6 +37,7 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+
 setupIonicReact();
 
 const App: React.FC = () => (
@@ -50,22 +54,36 @@ const App: React.FC = () => (
           <Route path="/tab3">
             <Tab3 />
           </Route>
+          <Route path="/tab4">
+            <Tab4 />
+          </Route>
+          <Route path="/tab5">
+            <Tab5 />
+          </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon color="success" icon={cashOutline} />
+            <IonLabel>Saldo</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon color="success" icon={checkmarkDoneCircleOutline} />
+            <IonLabel >Activas</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon color="warning" icon={closeCircleOutline} />
+            <IonLabel>Terminadas</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab4">
+            <IonIcon  color="primary" icon={fileTrayStackedOutline} />            
+            <IonLabel>Solicitudes</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab5" href="/tab5">
+            <IonIcon color="primary" icon={chatbubbleEllipsesOutline} />            
+            <IonLabel>Chat</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
