e4fddc2018f5cf526628ab5d0c0a485ee8a380f9
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-02-25 22:46:45 -0500

    Updated

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {  IonContent, IonHeader,IonPage  } from '@ionic/react';
+import {  IonContent, IonFab, IonFabButton, IonHeader,IonIcon,IonPage  } from '@ionic/react';
 // import {   flagOutline } from 'ionicons/icons';
 
 import Toolbar from '../components/Toolbar';
 import './Tab1.css';
 import DataDepRet from '../components/dataDepRet';
 import DataSaldoDepRet from '../components/dataSaldoDepRet';
+import { add } from 'ionicons/icons';
 
 
 
@@ -18,10 +19,16 @@ const Tab1: React.FC = () => {
           <Toolbar/>
       </IonHeader>
       <IonContent fullscreen >
+      {/* Data Saldo */}
       <DataSaldoDepRet/>
       {/* Data Depositos y Retiros*/}
       <DataDepRet/>
       {/* Data Depositos y Retiros*/}
+      <IonFab slot="fixed" horizontal="end" vertical="bottom">
+      <IonFabButton href='https://www.appsheet.com/start/01c021af-6861-4716-a17b-28d185b0cbbe' target='_blank'>
+        <IonIcon icon={add}></IonIcon>
+      </IonFabButton>
+    </IonFab>
 
       </IonContent>
     </IonPage>
